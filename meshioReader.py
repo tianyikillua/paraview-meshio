86b835939768b725a4038bf6ff2ca40863d86f8e
Tianyi Li <tianyikillua@gmail.com>
2019-10-27 14:08:01 +0100

    Expose field data

--- meshioReader.py
+++ meshioReader.py
@@ -14,7 +14,7 @@ __author__ = "Tianyi Li"
 __email__ = "tianyikillua@gmail.com"
 __copyright__ = "Copyright (c) 2019 {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.1.2"
+__version__ = "0.2.2"
 __status__ = "Development Status :: 4 - Beta"
 
 
@@ -125,5 +125,9 @@ class meshioReader(VTKPythonAlgorithmBase):
         for name in celldata_names:
             array = celldata_array(name)
             output.CellData.append(array, name)
+
+        # Field data
+        for name, array in mesh.field_data.items():
+            output.FieldData.append(array, name)
 
         return 1
