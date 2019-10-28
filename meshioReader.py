79fc254040e58c1ad22a89ee0163100b32d4792e
Tianyi Li <tianyikillua@gmail.com>
2019-10-28 20:07:33 +0100

    Correct version

--- meshioReader.py
+++ meshioReader.py
@@ -14,7 +14,7 @@ __author__ = "Tianyi Li"
 __email__ = "tianyikillua@gmail.com"
 __copyright__ = "Copyright (c) 2019 {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.2.2"
+__version__ = "0.2.1"
 __status__ = "Development Status :: 4 - Beta"
 
 
